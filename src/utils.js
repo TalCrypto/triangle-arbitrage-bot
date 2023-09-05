d16d65c1c10325a6b2245bbaadb23612d8d2e2c9
Emile Amajar <74051072+emilamaj@users.noreply.github.com>
2023-09-05 16:34:05 +0200

    Remove events that don't modify V3 liquidity: flash, collect. Easier on the rpc nodes.

--- src/utils.js
+++ src/utils.js
@@ -44,16 +44,16 @@ async function estimateNextBlockGas() {
     return estimate;
 }
 
-// Find pools that were probably updated in the given block
-async function findUpdatedPools(provider, blockNumber) {
+// Find pools that were updated in the given block
+async function findUpdatedPools(provider, blockNumber, pools) {
     // Selectors for events that update reserves in a Uniswap V2/V3 pool
     const eventSelectors = {
         V2Sync: ethers.utils.id('Sync(uint112,uint112)'), // [uint112 reserve0, uint112 reserve1]
         V3Mint: ethers.utils.id('Mint(address,address,int24,int24,uint128,uint256,uint256)'), // [address sender, address owner, int24 tickLower, int24 tickUpper, uint128 amount, uint128 amount0, uint128 amount1]
         V3Burn: ethers.utils.id('Burn(address,address,int24,int24,uint128,uint256,uint256)'), // [address sender, address owner, int24 tickLower, int24 tickUpper, uint128 amount, uint128 amount0, uint128 amount1]
         V3Swap: ethers.utils.id('Swap(address,address,int256,int256,uint160,uint128,int24)'), // [address sender, address recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick]
-        V3Collect: ethers.utils.id('Collect(address,int24,int24,uint128,uint128)'), // [address sender, int24 tickLower, int24 tickUpper, uint128 amount0, uint128 amount1]
-        V3Flash: ethers.utils.id('Flash(address,address,uint256,uint256,uint256,uint256)'), // [address sender, address recipient, uint256 amount0, uint256 amount1, uint256 paid0, uint256 paid1]
+        // V3Flash: ethers.utils.id('Flash(address,address,uint256,uint256,uint256,uint256)'), // [address sender, address recipient, uint256 amount0, uint256 amount1, uint256 paid0, uint256 paid1]
+        // V3Collect: ethers.utils.id('Collect(address,int24,int24,uint128,uint128)'), // [address sender, int24 tickLower, int24 tickUpper, uint128 amount0, uint128 amount1]
     }
 
     const filters = [];
@@ -73,7 +73,6 @@ async function findUpdatedPools(provider, blockNumber) {
     let poolAddresses = [];
     for (const logs of filterLogs) {
         for (const log of logs) {
-            console.log(log);
             poolAddresses.push(log.address);
             logCount++;
         }
